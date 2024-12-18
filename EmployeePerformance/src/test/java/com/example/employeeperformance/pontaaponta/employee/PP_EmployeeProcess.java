8c800a34a9b6e302209d946778afa66fc4ccfb0e
Talibert <guilhermetaliberti@gmail.com>
2024-12-17 21:08:36 -0300

    Implementando novo teste de PP para o processo de alterar um funcionário

--- EmployeePerformance/src/test/java/com/example/employeeperformance/pontaaponta/employee/PP_EmployeeProcess.java
+++ EmployeePerformance/src/test/java/com/example/employeeperformance/pontaaponta/employee/PP_EmployeeProcess.java
@@ -1,23 +1,13 @@
 package com.example.employeeperformance.pontaaponta.employee;
 
 import com.example.employeeperformance.EmployeeperformanceApplication;
-import com.example.employeeperformance.Fixtures.AttributeFixture;
 import com.example.employeeperformance.Fixtures.EmployeeFixture;
-import com.example.employeeperformance.Fixtures.EmployeePerformanceFixture;
-import com.example.employeeperformance.VOs.EmployeePerformanceVO;
 import com.example.employeeperformance.VOs.EmployeeVO;
-import com.example.employeeperformance.business.UpdatePerformance;
 import com.example.employeeperformance.controller.EmployeeController;
 import com.example.employeeperformance.entities.*;
-import com.example.employeeperformance.exceptions.notfound.EmployeeNotFoundException;
 import com.example.employeeperformance.repositories.AbstractRepositoryTests;
-import com.example.employeeperformance.repositories.AttributeRepository;
-import com.example.employeeperformance.repositories.EmployeePerformanceRepository;
 import com.example.employeeperformance.repositories.EmployeeRepository;
-import com.example.employeeperformance.services.ChangesRegistryService;
-import com.example.employeeperformance.services.EmployeePerformanceService;
 import com.example.employeeperformance.services.EmployeeService;
-import com.example.employeeperformance.types.AttributeType;
 import com.example.employeeperformance.types.SetorType;
 import com.example.employeeperformance.types.SituationType;
 import org.junit.jupiter.api.Assertions;
@@ -28,20 +18,20 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.time.Year;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
+/**
+ * Classe de testes ponta a ponta que envolvem os processos de funcionários
+ */
 @SpringBootTest(classes = EmployeeperformanceApplication.class)
 @ActiveProfiles("test")
-public class PP_CreateEmployee extends AbstractRepositoryTests {
+public class PP_EmployeeProcess extends AbstractRepositoryTests {
 
     @Autowired
     private EmployeeController employeeController;
+
+    @Autowired
+    private EmployeeService employeeService;
 
     @Autowired
     private EmployeeRepository employeeRepository;
@@ -50,6 +40,7 @@ public class PP_CreateEmployee extends AbstractRepositoryTests {
     public void init(){
         limpaBancos();
         resetID();
+        populaBanco();
     }
 
     public void limpaBancos(){
@@ -73,12 +64,9 @@ public class PP_CreateEmployee extends AbstractRepositoryTests {
         Assertions.assertEquals(response.getStatusCode().value(), 200);
         Assertions.assertNotNull(employeeVOSaved);
 
-        Optional<Employee> employeeSaved = employeeRepository.findById(employeeVOSaved.getId());
+        Employee employeeSaved = employeeService.findById(employeeVOSaved.getId());
 
-        if(employeeSaved.isPresent())
-            fazAssertDoFuncionarioSalvo(employeeSaved.get(), employeeVOParaSalvar);
-        else
-            throw new EmployeeNotFoundException("O usuário salvo não foi encontrado no banco!");
+        fazAssertDoFuncionarioSalvo(employeeSaved, employeeVOParaSalvar);
     }
 
     private void fazAssertDoFuncionarioSalvo(Employee employeeSaved, EmployeeVO employeeVOParaSalvar){
@@ -87,5 +75,48 @@ public class PP_CreateEmployee extends AbstractRepositoryTests {
         Assertions.assertEquals(employeeVOParaSalvar.getObservacao(), employeeSaved.getObservacao());
         Assertions.assertEquals(employeeVOParaSalvar.getSetorType(), employeeSaved.getSetorType());
         Assertions.assertEquals(employeeVOParaSalvar.getSituationType(), employeeSaved.getSituationType());
+    }
+
+    @Test
+    public void testeUpdateEmployee(){
+        Employee employeeAntigo = employeeService.findById(1L);
+        EmployeeVO employeeVO = new EmployeeVO();
+
+        employeeVO.setId(1L);
+        employeeVO.setNome("Yone");
+        employeeVO.setCpf("999.999.999-99");
+        employeeVO.setObservacao("Funcionário muito bom e dedicado que deve ser promovido!");
+        employeeVO.setSetorType(SetorType.STOCK);
+        employeeVO.setSituationType(SituationType.ATIVO);
+
+        ResponseEntity<?> response = employeeController.updateEmployee(employeeVO);
+
+        EmployeeVO employeeVOSaved = (EmployeeVO) response.getBody();
+
+        Assertions.assertEquals(response.getStatusCode().value(), 200);
+        Assertions.assertNotNull(employeeVOSaved);
+
+        Employee newEmployee = employeeService.findById(1L);
+
+        fazAssertDoFuncionarioAtualizado(newEmployee, employeeAntigo, employeeVO);
+    }
+
+    private void fazAssertDoFuncionarioAtualizado(Employee newEmployee, Employee employeeAntigo, EmployeeVO employeeVO){
+        Assertions.assertEquals(employeeVO.getCpf(), newEmployee.getCpf());
+        Assertions.assertEquals(employeeVO.getNome(), newEmployee.getNome());
+        Assertions.assertEquals(employeeVO.getObservacao(), newEmployee.getObservacao());
+        Assertions.assertEquals(employeeVO.getSetorType(), newEmployee.getSetorType());
+        Assertions.assertEquals(employeeVO.getSituationType(), newEmployee.getSituationType());
+
+        Assertions.assertNotEquals(employeeAntigo.getCpf(), newEmployee.getCpf());
+        Assertions.assertNotEquals(employeeAntigo.getNome(), newEmployee.getNome());
+        Assertions.assertNotEquals(employeeAntigo.getObservacao(), newEmployee.getObservacao());
+        Assertions.assertNotEquals(employeeAntigo.getSetorType(), newEmployee.getSetorType());
+    }
+
+    private void populaBanco(){
+        List<Employee> employeeList = EmployeeFixture.getEmployeeList();
+
+        employeeRepository.saveAll(employeeList);
     }
 }
